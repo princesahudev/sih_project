4698f2eed39f8a10b0416cddebdb7782b4de3afd
princesahudev <princesahu.mail@gmail.com>
2026-09-18 22:13:43 +0530

    Delete Farm2Door-main/Farm2Door-main directory

--- Farm2Door-main/Farm2Door-main/core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Account, Order, OrderItem, Product
-
-admin.site.register(Account)
-admin.site.register(Product)
-admin.site.register(Order)
-admin.site.register(OrderItem)
